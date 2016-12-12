d26da40daeec2a9b622d11e75fbf6509eb57c3b4
peiyee <tehpeiyee_1989@hotmail.com>
2016-12-12 22:35:56 +0800

    update package details

--- mix.exs
+++ mix.exs
@@ -7,25 +7,24 @@ defmodule Multipartable.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     description: "Build a simple or nested multipart form body",
+     package: package(),
+     deps: deps(),
+     source_url: "https://github.com/peiyee/elixir-multipartable"]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  defp package do
+    [
+      maintainers: ["Teh Pei Yee"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/peiyee/elixir-multipartable"}
+    ]
+  end
+
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
